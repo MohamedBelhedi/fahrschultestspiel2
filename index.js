3ca76eb7a022ebad04b6fa2d464e949ad3ebca6b
unknown <mbelhedi@outlook.com>
2022-03-23 23:38:40 +0100

    Fahrschulspiel Update 23.03.22 um 23:38 Uhr mit Javascript resposive und responsive css

---
+++ index.js
@@ -0,0 +1,32 @@
+function infosSpende()
+{
+    
+    var message=confirm('Danke für deine Spend bitte beachte die Datenschutzverordnung zum Theme Spenden')
+    
+    
+
+ 
+
+    if(message)
+    {
+
+
+        window.open("https://www.paypal.com");
+           
+
+        
+
+
+
+    }else
+    
+    {
+
+        return false;
+
+
+    }
+
+
+
+}
